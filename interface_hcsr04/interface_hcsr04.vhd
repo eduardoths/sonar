576ffe0792eb53b154dead98b3347fc11de6b29d
Eduardo Santos <46734503+eduardothsantos@users.noreply.github.com>
2021-10-26 09:40:50 -0300

    :bug: Fix: Interface hcsr04

--- interface_hcsr04/interface_hcsr04.vhd
+++ interface_hcsr04/interface_hcsr04.vhd
@@ -1,66 +1,83 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use IEEE.math_real.all;
 
-entity interface_hcsr04 is
-  port (
-    clock     : in std_logic;
-    reset     : in std_logic;
-    medir     : in std_logic;
-    echo      : in std_logic;
-    trigger   : out std_logic;
-    medida    : out std_logic_vector(11 downto 0); -- 3 digitos BCD
-    pronto    : out std_logic;
-    db_estado : out std_logic_vector(3 downto 0) -- estado da UC
-  );
+entity interface_hcsr04 is 
+	port ( 
+		clock:     in  std_logic;  
+		reset:     in  std_logic; 
+		medir:     in  std_logic; 
+		echo:      in  std_logic; 
+		trigger:   out std_logic; 
+		medida:    out std_logic_vector(11 downto 0); -- 3 digitos BCD 
+		pronto:    out std_logic; 
+		db_estado: out std_logic_vector(3 downto 0)   -- estado da UC 
+    ); 
 end interface_hcsr04;
 
-architecture interface_hcsr04_arch of interface_hcsr04 is
-  signal s_pulso_subida, s_pulso_descida, s_gera_pulso, s_mede_distancia, s_zera : std_logic;
+architecture hcsr04_arch of interface_hcsr04 is
 
-  component interface_hcsr04_uc
-    port (
-      clock, reset, medir                      : in std_logic;
-      pulso_subida, pulso_descida              : in std_logic;
-      gera_pulso, mede_distancia, pronto, zera : out std_logic;
-      db_estado                                : out std_logic_vector(3 downto 0)
-    );
-  end component;
+	component interface_hcsr04_uc 
+		port ( 
+			clock, reset, medir, echo: 					 in  std_logic;
+			zera, conta, pronto,limpa, registra, gera: out std_logic;
+			db_estado: 											 out std_logic_vector(3 downto 0)
+		);
+	end component;
 
-  component interface_hcsr04_fd
-    port (
-      clock, reset, echo, gera_pulso, mede_distancia, zera : in std_logic;
-      pulso_subida, pulso_descida                          : out std_logic;
-      trigger                                              : out std_logic;
-      medida                                               : out std_logic_vector(11 downto 0)
-    );
-  end component;
+	component interface_hcsr04_fd 
+		port (
+			clock, conta, zera: 		in  std_logic;
+			registra, gera, limpa:  in  std_logic;
+			trigger, fim :			   out std_logic;
+			distancia: 					out std_logic_vector (11 downto 0)
+		);
+	end component;
+	
+	component contadorg_m 
+		generic (
+			constant M: integer := 50 -- modulo do contador
+		);
+		port (
+			clock, zera_as, zera_s, conta: in std_logic;
+			Q: out std_logic_vector (natural(ceil(log2(real(M))))-1 downto 0);
+			fim, meio: out std_logic 
+		);
+	end component;
 
+	signal s_zera, s_conta, s_limpa, s_registra, s_gera: std_logic;
+	signal s_tick: std_logic;
+	signal tick_cm: std_logic;
+	signal tick_pl: std_logic;
+	signal s_zera_reset, s_limpa_reset : std_logic;
+	signal db_tick : std_logic;
+	signal config_unidade : std_logic;
 begin
-  uc : interface_hcsr04_uc port map(
-    clock,
-    reset,
-    medir,
-    s_pulso_subida,
-    s_pulso_descida,
-    s_gera_pulso,
-    s_mede_distancia,
-    pronto,
-    s_zera,
-    db_estado
-  );
+	config_unidade <= '0';
 
-  fd : interface_hcsr04_fd port map(
-    clock,
-    reset,
-    echo,
-    s_gera_pulso,
-    s_mede_distancia,
-    s_zera,
-    s_pulso_subida,
-    s_pulso_descida,
-    trigger,
-    medida
-  );
 
-end interface_hcsr04_arch;
+	-- unidade de controle
+	U1_UC: interface_hcsr04_uc port map (clock, reset, medir, echo,
+                                        s_zera, s_conta, pronto, s_limpa, s_registra, s_gera, db_estado);
+
+	-- fluxo de dados
+	s_zera_reset <= s_zera or reset;
+	s_limpa_reset <= s_limpa or reset;
+	U2_FD: interface_hcsr04_fd port map (clock, s_tick, s_zera_reset, s_registra, s_gera, s_limpa_reset,
+													 trigger, open, medida);
+			
+	-- gerador de tick
+	-- fator de divisao 58,82us para 20ns (2941=58,82us/20ns)		
+	U3_TICK_CM: contadorg_m  generic map (M => 2941) port map (clock, s_zera, '0', s_conta, open, tick_cm, open);
+	
+-- gerador de tick		
+	U4_TICK_PL: contadorg_m  generic map (M => 7471) port map (clock, s_zera, '0', s_conta, open, tick_pl, open);
+	
+	with config_unidade select
+	s_tick <= tick_cm when '0',
+				 tick_pl when '1',
+				 '0' when others;
+				 
+	db_tick <= s_tick;
+end architecture;
--- interface_hcsr04/interface_hcsr04_uc.vhd
+++ interface_hcsr04/interface_hcsr04_uc.vhd
@@ -3,92 +3,93 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity interface_hcsr04_uc is
-  port (
-    clock, reset, medir                      : in std_logic;
-    pulso_subida, pulso_descida              : in std_logic;
-    gera_pulso, mede_distancia, pronto, zera : out std_logic;
-    db_estado                                : out std_logic_vector(3 downto 0)
-  );
+	 port ( 
+		clock, reset, medir, echo: 					 in  std_logic;
+		zera, conta, pronto,limpa, registra, gera: out std_logic;
+		db_estado: 											 out std_logic_vector(3 downto 0)
+    );
+end entity;
 
-end interface_hcsr04_uc;
 
-architecture interface_hcsr04_uc_arch of interface_hcsr04_uc is
+architecture uc_arch of interface_hcsr04_uc is
 
-  type tipo_estado is (inicial, medindo, espera, conta, final);
-  signal Eatual : tipo_estado;
-  signal Eprox  : tipo_estado;
+    type tipo_estado is (inicial, preparacao, gera_trigger, espera_echo, medicao, armazena, final);
+    signal Eatual: tipo_estado;  -- estado atual
+    signal Eprox:  tipo_estado;  -- proximo estado
 
 begin
 
-  process (reset, clock)
-  begin
-    if reset = '1' then
-      Eatual <= inicial;
-    elsif clock'event and clock = '1' then
-      Eatual <= Eprox;
-    end if;
-  end process;
-
-  process (medir, pulso_subida, pulso_descida, Eatual)
-  begin
-
-    case Eatual is
-
-      when inicial =>
-        if medir = '0' then
-          Eprox <= inicial;
-        else
-          Eprox <= medindo;
+    -- memoria de estado
+    process (reset, clock)
+    begin
+        if reset = '1' then
+            Eatual <= inicial;
+        elsif clock'event and clock = '1' then
+            Eatual <= Eprox; 
         end if;
+    end process;
 
-      when medindo =>
-        Eprox <= espera;
+  -- logica de proximo estado
+    process (medir, echo, Eatual) 
+    begin
 
-      when espera =>
-        if pulso_subida = '0' then
-          Eprox <= espera;
-        else
-          Eprox <= conta;
-        end if;
+      case Eatual is
 
-      when conta =>
-        if pulso_descida = '0' then
-          Eprox <= conta;
-        else
-          Eprox <= final;
-        end if;
+        when inicial =>          if medir='1' then Eprox <= preparacao;
+                                 else                Eprox <= inicial;
+                                 end if;
 
-      when final =>
-        Eprox <= inicial;
+        when preparacao =>       Eprox <= gera_trigger;
+		  
+		  when gera_trigger =>     Eprox <= espera_echo;
 
-      when others =>
-        Eprox <= inicial;
+        when espera_echo =>      if echo='1' then   Eprox <= medicao;
+                                 else               Eprox <= espera_echo;
+                                 end if;
 
-    end case;
-  end process;
+        when medicao =>            if echo='1' then   Eprox <= medicao;
+                                 else               Eprox <= armazena;
+                                 end if;
+ 
+        when armazena =>         Eprox <= final;
+		  
+		  when final    =>			Eprox <= inicial;
+		  
+											
+        when others =>           Eprox <= inicial;
 
-  with Eatual select zera <=
-    '1' when medindo,
-    '0' when others;
-  
-  with Eatual select gera_pulso <=
-    '1' when medindo,
-    '0' when others;
-  
-  with Eatual select mede_distancia <=
-    '1' when conta,
-    '0' when others;
-  
-  with Eatual select pronto <=
-    '1' when final,
-    '0' when others;
+      end case;
 
-  with Eatual select db_estado <= 
-		"0000" when inicial,
-		"0001" when medindo,
-		"0010" when espera,
-		"0011" when conta,
-		"0100" when final,
-		"0000" when others;
-end interface_hcsr04_uc_arch;
-  
+    end process;
+
+    -- logica de saida (Moore)
+	 with Eatual select
+        limpa <= '1' when preparacao, '0' when others;
+		  
+    with Eatual select
+        zera <= '1' when preparacao, '0' when others;
+
+    with Eatual select
+        gera <= '1' when gera_trigger, '0' when others;
+
+    with Eatual select
+        conta <= '1' when medicao, '0' when others;
+
+    with Eatual select
+        registra <= '1' when armazena, '0' when others;
+		  
+    with Eatual select
+        pronto <= '1' when final, '0' when others;
+         
+	 -- saida de depuracao (db_estado)
+	 with Eatual select
+		  db_estado <= "0000" when inicial,
+						   "0001" when preparacao,
+							"0010" when gera_trigger,      
+							"0011" when espera_echo,     
+							"0100" when medicao,      
+							"0101" when armazena,
+							"0110" when final,
+							"1010" when others;
+
+end architecture;
